2fe0e966091e8ebea5425e4f0be677a550e6f73f
Avinash kumar <ak2783934@gmail.com>
2022-03-07 15:33:57 +0530

    whatsapp float

--- layout/index.js
+++ layout/index.js
@@ -1,9 +1,19 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import Link from "next/link";
 
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col w-full min-h-screen">
+      <div className="fixed cursor-pointer bottom-[40px] right-[40px]">
+        <Link href="http://whatsapp.com">
+          <img
+            src="/whatsapp-logo.png"
+            alt="whatsapp"
+            className="h-[60px] w-[60px]"
+          />
+        </Link>
+      </div>
       <Navbar />
       <div className="flex-grow">
         <main>{children}</main>
