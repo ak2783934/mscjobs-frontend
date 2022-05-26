1dcdd994dcf886fe906682ed292cd717ea98b501
Avinash kumar <ak2783934@gmail.com>
2022-05-26 14:16:56 +0530

    search feature implemented

--- components/SearchWithButton.js
+++ components/SearchWithButton.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const SearchWithButton = () => {
+  const [searchVal, setSearchVal] = useState("");
   return (
     <div className="flex flex-row w-full">
       <div className="h-6 w-[70%] md:h-10">
@@ -10,14 +14,23 @@ const SearchWithButton = () => {
             backgroundRepeat: "no-repeat",
             backgroundPosition: "5px 5px",
           }}
+          name="searchVal"
           placeholder="Search by profession"
+          onChange={(event) => setSearchVal(event.target.value)}
           className="w-full h-full px-3 pt-[2px] bg-[left_top_1rem] bg-[top_left_1rem] bg-[length:17px_17px] md:bg-[length:30px_30px] text-xs indent-3 md:indent-6 md:text-lg outline-0"
         />
       </div>
       <div className="h-6 w-[30%] md:h-10 bg-searchBg">
-        <button className="w-full h-full text-xs font-bold text-center text-white md:text-lg">
-          SEARCH
-        </button>
+        <Link
+          href={{
+            pathname: "/jobsearch",
+            query: { searchVal: searchVal },
+          }}
+        >
+          <button className="w-full h-full text-xs font-bold text-center text-white md:text-lg">
+            SEARCH
+          </button>
+        </Link>
       </div>
     </div>
   );
---
+++ pages/jobsearch/index.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../../layout";
+import Head from "next/head";
+import Link from "next/link";
+import BreadCrum from "../../components/BreadCrum";
+import Router, { useRouter } from "next/router";
+import { api } from "../api";
+
+const SearchJobPreview = ({ job }) => {
+  return (
+    <div className="px-4 py-1 mx-2 mt-3 bg-gray-300 rounded-xl">
+      <div className="text-lg">{job.companyName}</div>
+      <hr className="border-black border-1" />
+      <div className="px-2 text-xs md:px-8">{job.jobDescription}</div>
+      <div className="px-2 text-xs md:px-8">
+        Last Date of Application : {job.lastDateOfApplication}
+      </div>
+      <div className="px-2 text-xs md:px-8">
+        No of vacancies : {job.noOfOpenings}
+      </div>
+      <Link
+        href={{
+          pathname: "/jobPreviewPage",
+          query: { jobid: job._id, jobType: job.jobType1 },
+        }}
+      >
+        <div className="text-right cursor-pointer text-tiny">Explore more</div>
+      </Link>
+    </div>
+  );
+};
+
+const Index = () => {
+  const [jobs, setJobs] = useState([]);
+  const [queryData, setQueryData] = useState({});
+  useEffect(() => {
+    const data = Router.query;
+    console.log(data);
+    setQueryData(data);
+    api
+      .get(
+        `/jobsearch`,
+        {
+          params: {
+            searchVal: data.searchVal,
+          },
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        console.log(response.data);
+        setJobs(response.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+      });
+  }, []);
+  return (
+    <Layout>
+      <Head>
+        <title>MSC JOBS</title>
+        <link rel="icon" href="/msc-logo.svg" />
+        <meta name="MSC JOBS" content="Landing page of msc jobs" />
+        <meta name="Location" content="Bhubaneswar" />
+        <meta name="type" content="Consultancy" />
+        <meta name="jobs" content="government" />
+      </Head>
+      <div className="bg-[#008DF4] w-full">
+        <BreadCrum pageName="Search Jobs" />
+        <div className="flex flex-row py-4">
+          <div className="grow">
+            <div className="bg-white px-2 py-2 mx-12 md:mx-auto mb-2 md:mb-8 md:w-[800px] rounded">
+              <div className="text-sm font-medium md:text-xl">
+                Search Jobs : {queryData.searchVal}{" "}
+              </div>
+              <div className="flex flex-row py-1 text-xs md:text-sm">
+                Share this
+                <span>
+                  <img
+                    className="w-4 h-4 mx-1"
+                    src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-facebook-social-media-justicon-flat-justicon.png"
+                  />
+                </span>
+                <span>
+                  <img
+                    className="w-4 h-4 mx-1"
+                    src="https://img.icons8.com/color/48/000000/twitter--v1.png"
+                  />
+                </span>{" "}
+                <span>
+                  <img
+                    className="w-4 h-4 mx-1"
+                    src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-linkedin-social-media-justicon-flat-justicon.png"
+                  />
+                </span>
+              </div>
+              <hr />
+              <div className="mx-4 md:mx-8">
+                <div className="h-[400px] overflow-y-auto ">
+                  {jobs.map((job, Index) => {
+                    return <SearchJobPreview key={Index} job={job} />;
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Index;
